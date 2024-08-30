1c765aca5a0c354422ac4319b110a67f27b38904
Md Zartaj Afser <zartaj.afsar@gmail.com>
2024-08-30 17:24:49 +0530

    Added from and to timestamp method

--- scripts/FetchPushStake.js
+++ scripts/FetchPushStake.js
@@ -13,22 +13,32 @@ const epochDuration = 150276;
 
 let fromBlock;
 let toBlock;
+let FromTimestamp;
+let ToTimestamp;
+
 let TotalAmountsStaked = 0n;
 let provider;
 
+const averageBlockTime = 12; // Average block time for Ethereum
+
 async function main(_user) {
-  //create Provider 
-   provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+  //create Provider
+  provider = new ethers.JsonRpcProvider(
+    `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+  );
 
   console.log("Initiating from and to with current Epoch");
   //get the currentEpoch
   let _epoch = await getCurrentEpoch();
   //pass the current epoch here
   [fromBlock, toBlock] = getFromAndToBlock(_epoch);
+  [FromTimestamp, ToTimestamp] = await getFromAndToTimestamp(_epoch);
+
+  console.log(FromTimestamp, ToTimestamp);
 
   let Core = new ethers.Contract(core, abi, provider);
   console.log("Fetching Staked events from core");
-  
+
   // Create the filter for the specific user address
   let eventFilter = Core.filters.Staked(_user);
   let events = await Core.queryFilter(eventFilter, fromBlock, toBlock);
@@ -40,21 +50,56 @@ async function main(_user) {
     TotalAmountsStaked += event.args.amountStaked;
   });
 
-  console.log(
-    `Total Amount staked in epoch ${_epoch}: ${TotalAmountsStaked}`
-  );
+  console.log(`Total Amount staked in epoch ${_epoch}: ${TotalAmountsStaked}`);
 }
 //To get from and to Block number for a given epoch
 function getFromAndToBlock(epoch) {
-  let fromBlock=  genesisBlock + epochDuration * epoch - epochDuration;
-  let ToBlock = genesisBlock + epochDuration * epoch - 1
+  let fromBlock = genesisBlock + epochDuration * epoch - epochDuration;
+  let ToBlock = genesisBlock + epochDuration * epoch - 1;
   return [fromBlock, ToBlock];
 }
 
-//To get the cuurent epoch 
+async function getFromAndToTimestamp(epoch) {
+  let fromBlock = genesisBlock + epochDuration * epoch - epochDuration;
+  let toBlock = genesisBlock + epochDuration * epoch - 1;
+
+  let FromTimestamp;
+  let ToTimestamp;
+
+  const latestBlock = await provider.getBlock("latest");
+
+  if (latestBlock.number >= fromBlock) {
+    let block = await provider.getBlock(fromBlock);
+    FromTimestamp = block.timestamp;
+  } else {
+    const fromTarget = fromBlock - latestBlock.number;
+    // Estimate the from block timestamp
+    FromTimestamp = latestBlock.timestamp + (fromTarget * averageBlockTime);
+  }
+
+  if (latestBlock.number >= toBlock) {
+    let block = await provider.getBlock(toBlock);
+    ToTimestamp = block.timestamp;
+  } else {
+    const toTarget = toBlock - latestBlock.number;
+    // Estimate the from block timestamp
+    ToTimestamp = latestBlock.timestamp + (toTarget * averageBlockTime);
+  }
+
+  console.log(
+    `Estimated Timestamp: ${new Date(FromTimestamp * 1000).toLocaleString()}`
+  );
+  console.log(
+    `Estimated Timestamp: ${new Date(ToTimestamp * 1000).toLocaleString()}`
+  );
+
+  return [FromTimestamp, ToTimestamp];
+}
+
+//To get the cuurent epoch
 async function getCurrentEpoch() {
   let currentBlock = await provider.getBlockNumber();
-  let epoch =  (currentBlock - genesisBlock) / epochDuration + 1;
+  let epoch = (currentBlock - genesisBlock) / epochDuration + 1;
   return ~~epoch;
 }
 
